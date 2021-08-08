dda2383f8f3dc3b664ad942f9af576c3ab8285ad
Carlos V <cavarod@gmail.com>
2021-08-08 10:22:53 -0700

    finished 21.2.4

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,11 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql` 
+	type Auth {
+		token: ID!
+		user: User
+	}
+
 	type Thought {
 		_id: ID
 		thoughtText: String
@@ -33,6 +38,11 @@ const typeDefs = gql`
 		user(username: String!): User
   		thoughts(username: String): [Thought]
   		thought(_id: ID!): Thought
+	}
+
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
 	}
 `;
 
